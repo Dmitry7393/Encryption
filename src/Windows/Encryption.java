6da15143f5f1651e7f52f56a5b60918fe07e5267
Dmitry7393 <dmitry7393@gmail.com>
2016-03-13 16:30:26 +0200

    GridBagLayout

--- src/Windows/Encryption.java
+++ src/Windows/Encryption.java
@@ -4,29 +4,38 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class Encryption {
+	private JLabel str_key = new JLabel("Key");
 	private JTextField jtf;
-	  GridBagConstraints gbc = new GridBagConstraints();
+	  GridBagConstraints gbc ;
 	  
 	 Encryption()
 	{
-		JFrame jfrm = new JFrame("Encryption");
-	//jfrm.setLayout(new FlowLayout());
-		   jfrm.setLayout(new GridBagLayout());
-		 
-		jfrm.setSize(720, 400);
-		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		jtf = new JTextField(16);
+		JPanel panel = new JPanel(new GridBagLayout());
+	    gbc = new GridBagConstraints();
+	    gbc.insets = new Insets(0,20,20,0);
+	  //  gbc.weightx = 1.0;
+	    gbc.anchor = GridBagConstraints.NORTHWEST;
+	  // gbc.gridwidth = GridBagConstraints.REMAINDER;
+	    
+	   
+	   
+	    JFrame jfrm = new JFrame("Encryption");
+		   jfrm.setSize(720, 400);
+		   jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		jtf = new JTextField(12);
 		JLabel jlab = new JLabel("message");
 		JButton jbtn1 = new JButton("Encrypt");
 		JButton jbtn2 = new JButton("Decrypt");
@@ -52,20 +61,26 @@ public class Encryption {
 		});
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		jfrm.add(jtf, gbc);
-		gbc.gridx = 4;
-		gbc.gridy = 0;
-		jfrm.add(jbtn1, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		jfrm.add(jbtn2, gbc);
+		panel.add(str_key, gbc);
 		
-		gbc.gridx = 4;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		panel.add(jtf, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy =  1;
+
+		gbc.ipady = 20;
+		gbc.ipadx = 20;
+		panel.add(jbtn1, gbc);
+		gbc.gridx = 2;
 		gbc.gridy = 1;
-		jfrm.add(jlab, gbc);
-		gbc.gridx = 3;
-		gbc.gridy = 3;
-		 
+		panel.add(jbtn2, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		panel.add(jlab, gbc);
+		   jfrm.add(panel);
 		jfrm.setVisible(true);
 	}
 	public static void main(String[] args) {
