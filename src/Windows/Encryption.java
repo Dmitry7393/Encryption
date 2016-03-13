557fb95f7f8ac94f8129dbf0aa076b9391ec9b19
Dmitry7393 <dmitry7393@gmail.com>
2016-03-13 19:16:00 +0200

    Load file to encrypt
    I have used JFileChooser to choose ciphertext

--- src/Windows/Encryption.java
+++ src/Windows/Encryption.java
@@ -5,8 +5,13 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,11 +25,13 @@ import AES.Encrypt;
 
 public class Encryption {
 	private JLabel str_key = new JLabel("Key");
-	private JTextField jtf ;
+	private JLabel str_plaintext = new JLabel("Plaintext");
+	private JTextField jtf_plaintext ;
+	private JTextField jtf_key ;
 	private DefaultTableModel model = new DefaultTableModel(); 
 	private JTable table = new JTable(model); 
+	private JButton open_file = new JButton("Open file to decrypt");
 	  GridBagConstraints gbc ;
-	  
 	 Encryption()
 	{
 		JPanel panel = new JPanel(new GridBagLayout());
@@ -40,8 +47,10 @@ public class Encryption {
 		   jfrm.setSize(720, 400);
 		   jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		jtf = new JTextField(12);
-		jtf.setText("Thats my Kung Fu");
+		   jtf_key = new JTextField(12);
+		   jtf_plaintext = new JTextField(12);
+		jtf_key.setText("Thats my Kung Fu");
+		jtf_plaintext.setText("Two One Nine Two");
 		JLabel jlab = new JLabel("message");
 		JButton jbtn1 = new JButton("Encrypt");
 		JButton jbtn2 = new JButton("Decrypt");
@@ -54,9 +63,8 @@ public class Encryption {
 			{
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					
-					String str_plain_text = "Two One Nine Two";
-					String str_key = jtf.getText();
+					String str_plain_text = jtf_plaintext.getText();
+					String str_key = jtf_key.getText();
 					Encrypt a = new Encrypt(str_plain_text, str_key);
 					byte[][] r = a.get_ciphertext();
 					
@@ -101,37 +109,63 @@ public class Encryption {
 						plain_text = plain_text + (String) table.getModel().getValueAt(i, j) + " ";
 					}
 				}
-				Decrypt d = new Decrypt(plain_text, jtf.getText());
+				Decrypt d = new Decrypt(plain_text, jtf_key.getText());
 				String source_text = d.get_text();
 				jlab.setText(source_text);
 			}
 			
 		});
+		open_file.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser fileChooser = new JFileChooser("D:\\");
+				//fileChooser.setCurrentDirectory("D:/");
+				if (fileChooser.showOpenDialog(open_file) == JFileChooser.APPROVE_OPTION) {
+				  File file = fileChooser.getSelectedFile();
+				 String plain_text = read_file(file.getPath());
+				 Decrypt d = new Decrypt(plain_text, jtf_key.getText());
+					String source_text = d.get_text();
+					jlab.setText(source_text);
+				}
+			}
+			
+		});
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		panel.add(str_plaintext, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
 		panel.add(str_key, gbc);
 		
 		gbc.gridx = 1;
 		gbc.gridy = 0;
-		panel.add(jtf, gbc);
+		panel.add(jtf_plaintext, gbc);
 		
 		gbc.gridx = 1;
-		gbc.gridy =  1;
-
-		gbc.ipady = 20;
-		gbc.ipadx = 20;
-		panel.add(jbtn1, gbc);
-		gbc.gridx = 2;
 		gbc.gridy = 1;
-		panel.add(jbtn2, gbc);
+		panel.add(jtf_key, gbc);
 		
+		gbc.gridx = 0;
+		gbc.gridy =  2;
+		gbc.ipady = 10;
+		gbc.ipadx = 10;
+		panel.add(jbtn1, gbc);
 		gbc.gridx = 1;
 		gbc.gridy = 2;
+		panel.add(jbtn2, gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 0;
 		panel.add(jlab, gbc);
 		
 		gbc.gridx = 1;
 		gbc.gridy = 3;
 		panel.add(table, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		panel.add(open_file, gbc);
 		   jfrm.add(panel);
 		jfrm.setVisible(true);
 	}
@@ -144,5 +178,29 @@ public class Encryption {
 			}
 		});
 	}
+	private static String read_file(String path)
+    {
+		BufferedReader br = null;
+		String source_string = "";
+		try {
+			String current_line = "";
+			br = new BufferedReader(new FileReader(path));
+			while ((current_line = br.readLine()) != null) 
+			{
+				System.out.println("input1: " + current_line);
+				source_string = current_line;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return source_string;
+    }
 
 }
