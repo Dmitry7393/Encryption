a6016074b53dede64fe1516751e6d4293c2b7da4
Dmitry7393 <dmitry7393@gmail.com>
2016-05-09 00:22:59 +0300

    Added TrayIcon

--- src/Windows/Encryption.java
+++ src/Windows/Encryption.java
@@ -1,12 +1,30 @@
 package Windows;
 
+import java.awt.AWTException;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
 
 public class Encryption {
-	Encryption() throws IOException {
+	private SystemTray systemTray = SystemTray.getSystemTray();
+	private TrayIcon trayIcon;
+	String prefix = "foobar";
+	String suffix = ".tmp";
+
+	Encryption() throws IOException, URISyntaxException {
+
 		JFrame frame = new JFrame("Encryption");
 		frame.setSize(920, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,6 +39,43 @@ public class Encryption {
 		tabbedPane.addTab("RSA", new TabRSA(frame.getLocation().x, frame.getLocation().y));
 		frame.add(tabbedPane);
 		frame.setVisible(true);
+
+		// Create a temporary file
+		File tempFileImageLock = null;
+		try {
+			tempFileImageLock = File.createTempFile("imgtrlock", ".tmp");
+			tempFileImageLock.deleteOnExit();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		FileOutputStream fos = new FileOutputStream(tempFileImageLock.getAbsolutePath());
+		int value = 0;
+		InputStream inputStreamImage = Encryption.class.getResourceAsStream("/resources/image_tray_lock4.png");
+		while ((value = inputStreamImage.read()) != -1) {
+			fos.write((byte) value);
+		}
+		fos.close();
+		inputStreamImage.close();
+
+		trayIcon = new TrayIcon(ImageIO.read(tempFileImageLock), "Encryption");
+
+		trayIcon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(true);
+				frame.setState(JFrame.NORMAL);
+				removeTrayIcon();
+			}
+		});
+
+		frame.addWindowStateListener(new WindowStateListener() {
+			public void windowStateChanged(WindowEvent e) {
+				if (e.getNewState() == JFrame.ICONIFIED) {
+					frame.setVisible(false);
+					addTrayIcon();
+				}
+			}
+		});
 	}
 
 	public static void main(String[] args) {
@@ -30,18 +85,25 @@ public class Encryption {
 					new Encryption();
 				} catch (IOException e) {
 					e.printStackTrace();
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
 		});
 	}
-	/*
-	 * private static String read_file(String path) { BufferedReader br = null;
-	 * String source_string = ""; try { String current_line = ""; br = new
-	 * BufferedReader(new FileReader(path)); while ((current_line =
-	 * br.readLine()) != null) { System.out.println("input1: " + current_line);
-	 * source_string = current_line; } } catch (IOException e) {
-	 * e.printStackTrace(); } finally { try { if (br != null) br.close(); }
-	 * catch (IOException ex) { ex.printStackTrace(); } } return source_string;
-	 * }
-	 */
+
+	private void removeTrayIcon() {
+		systemTray.remove(trayIcon);
+	}
+
+	private void addTrayIcon() {
+		try {
+			systemTray.add(trayIcon);
+			trayIcon.displayMessage("Encryption", "Window minimised to tray, double click to show",
+					TrayIcon.MessageType.INFO);
+		} catch (AWTException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
